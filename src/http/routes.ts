d701c0af963d3b4930f8fdf11a813c95f04c70d7
Flavio <fjcf@poli.br>
2023-10-05 18:03:21 -0300

    feat: add sessions authenticate route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { RegisterController } from './controllers/RegisterController'
+import { AuthenticateController } from './controllers/AuthenticateController'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', RegisterController)
+  app.post('/sessions', AuthenticateController)
 }
