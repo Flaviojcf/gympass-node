8924cbe00cdd1252d0331b0f4c01f4e0a1ab8e60
Flavio <fjcf@poli.br>
2023-10-05 16:53:21 -0300

    feat: create in memory users repository

---
+++ src/repositories/inMemory/inMemoryUsersRepository.ts
@@ -0,0 +1,27 @@
+import { User, Prisma } from '@prisma/client'
+import { IUsersRepository } from '../interfaces/IUsersRepository'
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: '1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+    this.items.push(user)
+    return user
+  }
+}
