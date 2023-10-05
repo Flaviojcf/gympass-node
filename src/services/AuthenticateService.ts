6bd133de646d961fcc4016851c5102221c750fa9
Flavio <fjcf@poli.br>
2023-10-05 17:44:21 -0300

    feat: create Authenticate service

---
+++ src/services/AuthenticateService.ts
@@ -0,0 +1,38 @@
+import { IUsersRepository } from '@/repositories/interfaces/IUsersRepository'
+import { InvalidCredentialsError } from './errors/InvalidCredentialsError'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+interface IAuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface IAuthenticateServiceResponse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: IAuthenticateServiceRequest): Promise<IAuthenticateServiceResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPAsswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPAsswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
